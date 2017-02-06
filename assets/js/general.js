4e739c45493eb1bce7f1e8cf34810a82f4b753ee
David Balderston <dbalders@gmail.com>
2017-02-05 19:55:05 -0800

    update

--- assets/js/general.js
+++ assets/js/general.js
@@ -22,18 +22,24 @@ function setCoverImage(coverImg, landscape) {
             coverImg.css('height', 'auto');
         }
         if ($(window).width() < 500) {
-            console.log(coverImg.height());
-            console.log(coverImg.width());
-            coverImg.css('margin-left', '-25%')
+            //If the width of the cover image is less than 1.5 times width of the screen
+            if (coverImg.width() < ($(window).width() * 1.5)) {
+                coverImg.css('margin-left', '0');
+            } else {
+                coverImg.css('margin-left', '-25%')
+            }
         } else {
             coverImg.css('margin-left', '0')
         }
     }
-    if (coverImg.height() < 575) {
-        coverImg.css('margin-top', '0');
-    }
-    if (coverImg.width() < ($(window).width() * 1.5)) {
-        coverImg.css('margin-left', '0');
+
+    if (landscape === false) {
+        //If the height of the cover image is less than 1.5 times height of the image div
+        if (coverImg.height() < 690) {
+            coverImg.css('margin-top', '0');
+        } else {
+            coverImg.css('margin-top', '-25%');
+        }
     }
     coverImg.show();
 }
