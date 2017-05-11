62d546048428160917e2e6f9609ae818f3aaf001
David Balderston <dbalders@gmail.com>
2017-05-11 16:06:53 -0700

    fix latest posts too

--- assets/js/general.js
+++ assets/js/general.js
@@ -13,14 +13,6 @@ function fluidBoxInit() {
 
 function setCoverImage(coverImg, landscape) {
     if (landscape === true) {
-        if (coverImg.height() <= 460) {
-            coverImg.css('width', 'auto');
-            coverImg.css('height', '100%');
-        }
-        if (coverImg.width() < $(window).width()) {
-            coverImg.css('width', '100%');
-            coverImg.css('height', 'auto');
-        }
         if ($(window).width() < 500) {
             //If the width of the cover image is less than 1.5 times width of the screen
             if (coverImg.width() < ($(window).width() * 1.5)) {
